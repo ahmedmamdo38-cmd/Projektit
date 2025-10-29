ecf603ab66f1ce1264eff9ce173418fc71d9f384
haben <tesfahiwet869@gmail.com>
2025-10-29 14:20:41 +0200

    test

---
+++ sytle.js
@@ -0,0 +1,16 @@
+function tyhjenna(){
+    document.getElementById("name").value = "";
+    document.getElementById("gmail").value = "";
+    document.getElementById("salasana").value = "";
+    document.getElementById("radio1").value = "";
+    document.getElementById("check1").value = "";
+    document.getElementById("check2").value = "";
+    document.getElementById("check3").value = "";
+    document.getElementById("aihe").value = "";
+    document.getElementById("kerro").value = "";
+
+}
+
+function laheta(){
+    
+}
